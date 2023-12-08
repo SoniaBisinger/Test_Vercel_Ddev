a31d6fb1116d3316fec32831c4171882155f40f5
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-12-08 15:31:54 +0100

    Trying to fix for Vercel

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -14,23 +14,20 @@ from pathlib import Path
 import os
 import environ
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+env = environ.Env()
+environ.Env.read_env()
 
 # Set the project base directory
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 # False if not in os.environ because of casting above
-DEBUG = env('DEBUG')
+DEBUG = False
 
 # Raises Django's ImproperlyConfigured
 # exception if SECRET_KEY not in os.environ
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', 'composeexample']
 
